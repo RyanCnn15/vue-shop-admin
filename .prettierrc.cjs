e4272b620bed8d3167799cec2aa5e8a408d239ba
cj <chenji@valuelinkltd.com>
2023-05-22 09:19:45 +0800

    style: 更新 prettier 配置

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,20 +1,35 @@
 module.exports = {
+  // 单行长度
   printWidth: 100,
+  // 缩进长度
   tabWidth: 2,
+  // 使用 Tab 代替空格缩进
   useTabs: false,
+  // 句末添加分号
   semi: true,
+  // 使用单引号
   singleQuote: true,
+  // 仅在需要时给对象的 key 添加引号
   quoteProps: 'as-needed',
+  // jsx 中使用单引号
   jsxSingleQuote: false,
+  // 多行时是否打印尾随逗号
   trailingComma: 'none',
+  // 在对象前后添加空格 - eg: { foo: bar }
   bracketSpacing: true,
-  jsxBracketSameLine: false,
+  // 给箭头函数的参数添加括号
   arrowParens: 'always',
+  // format 执行的范围
   rangeStart: 0,
   rangeEnd: Infinity,
+  // 无需顶部注释即可格式化
   requirePragma: false,
+  // 在已被preitter格式化的文件顶部加上标注
   insertPragma: false,
+  // 文章换行
   proseWrap: 'preserve',
+  // html 中的空格敏感性
   htmlWhitespaceSensitivity: 'css',
+  // 行尾换行符
   endOfLine: 'lf'
 };
--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,39 @@
-import vue from "@vitejs/plugin-vue";
-import path from "path";
-import { defineConfig } from "vite";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import WindiCSS from "vite-plugin-windicss";
-import ElementPlus from "unplugin-element-plus/vite"; // 按照官网自动引入没有样式，这里要多添加一个插件
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import WindiCSS from 'vite-plugin-windicss';
+import ElementPlus from 'unplugin-element-plus/vite'; // 按照官网自动引入没有样式，这里要多添加一个插件
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: "0.0.0.0", // 服务器主机名，如果允许外部访问，可设置为"0.0.0.0"
+    host: '0.0.0.0', // 服务器主机名，如果允许外部访问，可设置为"0.0.0.0"
     open: true,
     proxy: {
-      "/api": {
-        target: "http://ceshi13.dishait.cn",
+      '/api': {
+        target: 'http://ceshi13.dishait.cn',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
+      '@': path.resolve(__dirname, 'src')
+    }
   },
   plugins: [
     vue(),
     WindiCSS(),
     ElementPlus(),
     AutoImport({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
+      resolvers: [ElementPlusResolver()]
+    })
+  ]
 });
